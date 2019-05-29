eec5472b4ae2d494f9baf12a466489f53b11d412
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-29 13:12:27 -0300

    W13 - Ativando a câmera com API MediaDevices.getUserMedia()

--- src/controller/whatsapp.controller.js
+++ src/controller/whatsapp.controller.js
@@ -194,7 +194,9 @@ class WhatsAppController {
             this.el.panelCamera.addClass('open');
             this.el.panelCamera.css({
                 'height': ' calc(100% - 120px)'
-            })
+            });
+
+            this._camera = new CameraController(this.el.videoCamera);
         });
 
         this.el.btnClosePanelCamera.on('click', e => {
