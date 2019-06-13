80c6551699cf006405d98bf393a6cab58cfa2ff5
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-13 13:20:28 -0300

    W47 - Atualizando ultima mensagem enviada manualmente

--- src/controller/whatsapp.controller.js
+++ src/controller/whatsapp.controller.js
@@ -85,7 +85,7 @@ export class WhatsAppController {
                                     <span dir="auto" title="${contact.name}" class="_1wjpf">${contact.name}</span>
                                 </div>
                                 <div class="_3Bxar">
-                                    <span class="_3T2VG">${contact.lastMessageTime}</span>
+                                    <span class="_3T2VG">${Format.timestampToTime(contact.lastMessageTime)}</span>
                                 </div>
                             </div>
                             <div class="_1AwDx">
